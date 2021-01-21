ab5e7e49bb03cfbdd5359f7ba5e484e74c840773
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2021-01-21 10:55:21 -0500

    Add favicon

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import HomePage from "./containers/Homepage";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ amazon/src/containers/Homepage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const HomePage = (props) => {
+  return(
+        <div>
+            Home
+        </div>
+    )
+ }
+
+export default HomePage
