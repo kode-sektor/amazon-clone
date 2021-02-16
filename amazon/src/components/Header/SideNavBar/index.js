a805401fc98b601ef6de14b91a8217131d1a5aa9
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2021-02-15 23:54:36 -0500

    Set basic markup for Side Nav Menu

--- amazon/src/components/Header/SideNavBar/index.js
+++ amazon/src/components/Header/SideNavBar/index.js
@@ -4,14 +4,33 @@ import { Link } from 'react-router-dom';
 
 import SideNav from 'react-simple-sidenav'
 
+import './index.css'
 
-const SideNavBar = ({showNav, onHideNav}) => {
+const SideNavBar = ({ showNav, onHideNav }) => {
     return (
         <SideNav 
             showNav={showNav}
             onHideNav={onHideNav}
         >
-            <li><Link to="/">Rate us</Link></li>
+            <div className="side__nav">
+                <span class="side__nav__close__icon"></span>
+                <section>
+                    <a className="side__nav__customer__profile">
+                        <span className="side__nav__avatar"></span>
+                        <b className="side__nav__customer__name">Hello, Kay</b>
+                    </a>
+                </section>
+                <section className="side__nav__menu">
+                    <ul className="side__nav__menu__trending">
+                        <li>
+                            <h4 className="side__nav__menu__title side__nav__item">Trending</h4>
+                        </li>
+                        <li>
+                            <a className="side__nav__item" href="/best-sellers">Best Sellers</a>
+                        </li>
+                    </ul>
+                </section>
+            </div>
         </SideNav>
     )
 }
