047c7a165a82abeff046db450bf2a9d6305b1021
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2021-02-21 23:59:54 -0500

    Add Card Modules 3 & 4 and for prime Video

--- amazon/src/components/UI/Card/index.js
+++ amazon/src/components/UI/Card/index.js
@@ -2,12 +2,11 @@ import React, { useState } from 'react'
 
 
 const Card_One = ({name, date, img} ) => {
-
     return (
         <section className="sub__layout__card__grid">
             <div className="sub__layout__inner">
                 <h2 className="sub__layout__inner__header">
-                    <a className="sub__layout__inner__header__avatar">
+                    <a href="/user-profile" className="sub__layout__inner__header__avatar">
                         <img className="user__avatar" src={img[0].avatar} alt="profile image" />
                     </a>
                     <div className="sub__layout__inner__header__greeting">
@@ -61,8 +60,35 @@ const Card_One = ({name, date, img} ) => {
     )
 }
 
+const Card_Two = ({title, img, date}) => {
+    return (
+        <section className="sub__layout__card__grid">
+            <div className="sub__layout__inner">
+                <h2 className="sub__layout__inner__header">
+                    <span className="sub__layout__inner__header__title">{title}</span>
+                </h2>
+                <section className="sub__layout__inner__body">
+                    <a href={img[0].link}>
+                        <div className="sub__layout__inner__body__fluid__img">
+                            <img src={img[0].href} alt={img[0].alt} />
+                        </div>
+                    </a>
+                    <div className="sub__layout__inner__timeline">
+                        <div class="sub__layout__inner__timelinedot"></div>
+                        <div class="sub__layout__inner__timelineline"></div>
 
-const Card_Two = ({title, img, alt, link, linktxt}) => {
+                        <p class="sub__layout__inner__timelinedate">{date}</p>
+                        <p className="sub__layout__card__grid__footer">
+                            <a href="/history">See your browsing history</a>
+                        </p>
+                    </div>
+                </section>
+            </div>
+        </section>
+    )
+}
+
+const Card_Three = ({title, img, alt, link, linktxt}) => {
     return (
         <section className="sub__layout__card__grid">
             <div className="sub__layout__inner">
@@ -86,4 +112,52 @@ const Card_Two = ({title, img, alt, link, linktxt}) => {
     )
 }
 
-export { Card_One, Card_Two }
+const Card_Four = ({title, text, catLink}) => {
+    return (
+        <section className="sub__layout__card__grid gift">
+            <div className="sub__layout__inner">
+                <h2 className="sub__layout__inner__header">
+                    <span className="sub__layout__inner__header__title">{title}</span>
+                </h2>
+                <section className="sub__layout__inner__body">
+                    <p>{text}</p>
+                    <p className="sub__layout__card__grid__footer">
+                        <a href={catLink}>Explore Now</a>
+                    </p>
+                </section>
+            </div>
+        </section>
+    )
+}
+
+
+const Card_Video = ({title, img }) => {
+    return (
+        <section className="sub__layout__card__grid grid__half">
+            <div className="prime__video">
+                <h2 className="prime__video__header">
+                    <span className="prime__video__title">Prime Video: Recommended for you</span>
+                    <span className="prime__video__maintitle">{title}</span>
+                </h2>
+                <section className="prime__video__ui__card">
+                    <div className="prime__video__img"></div>
+                    <a href="/">
+                        <div className="prime__video__imgLink">
+                            <img className="prime__video__image" src={img} alt={title}/>
+                        </div>
+                        <div className="prime__gradient"></div>
+                    </a>
+                </section>
+                <div className="sub__layout__inner__timeline">
+                    <p className="sub__layout__card__grid__footer">
+                        <a href={`/prime-video/${title}`}>Start watching on Prime Video</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+
+export { Card_One, Card_Two, Card_Three, Card_Four, Card_Video }
--- amazon/src/containers/Home/index.js
+++ amazon/src/containers/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../../components/Layout/index'
 
-import { Card_One, Card_Two } from '../../components/UI/Card'
+import { Card_One, Card_Two, Card_Three, Card_Four, Card_Video } from '../../components/UI/Card'
 
 import Slider from './Carousel'
 
@@ -9,12 +9,13 @@ import './index.css'
 
 import user_avatar from '../../images/user_avatar.jpg'
 import card_gifting from '../../images/card_gifting.jpg'
-import electronics_headphones from '../../images/electronics_headphones.jpg'
-import home_kitchen from '../../images/home_kitchen.jpg'
+import electronics_headphones from '../../images/categories/electronics_headphones.jpg'
+import home_kitchen from '../../images/categories/home_kitchen.jpg'
 import your_orders from '../../images/your_orders.jpg'
 import desktop_small_big_card from '../../images/desktop_small_big_card.jpg'
 import desktop_deals_pets from '../../images/desktop_deals_pets.jpg'
 import home_and_kitchen from '../../images/home_and_kitchen.jpg'
+import sandwich_toaster from '../../images/products/sandwich_toaster.jpg'
 
 const Home = (props) => {
   return(
@@ -51,76 +52,39 @@ const Home = (props) => {
                             }
                         ]}
                     />
-                    <section className="sub__layout__card__grid">
-                        <div className="sub__layout__inner">
-                            <h2 className="sub__layout__inner__header">
-                                <span className="sub__layout__inner__header__title">Recently Viewed</span>
-                            </h2>
-                            <section className="sub__layout__inner__body">
-                                <a href="/gp/product/B07MM2RVM5">
-                                    <div className="sub__layout__inner__body__fluid__img">
-                                        <img alt="Gotham Steel Sandwich Maker, Toaster and Electric Panini Grill with Ultra Nonstick Copper Surface - Makes 2 Sandwiches in Minutes with Virtually No Clean Up, with Easy Cut Edges and Indicator Lights" src="https://m.media-amazon.com/images/I/41S4lanqmIL._AC_SL260_.jpg"/>
-                                    </div>
-                                </a>
-                                <div className="sub__layout__inner__timeline">
-                                    <div class="sub__layout__inner__timelinedot"></div>
-                                    <div class="sub__layout__inner__timelineline"></div>
-
-                                    <p class="sub__layout__inner__timelinedate">Sun, Feb 14</p>
-                                    <p className="sub__layout__card__grid__footer">
-                                        <a href="/history">See your browsing history</a>
-                                    </p>
-                                </div>
-                            </section>
-                        </div>
-                    </section>
-                    <Card_Two
+                    <Card_Two 
+                        title={"Recently Viewed"}
+                        img={[
+                            {
+                                href : sandwich_toaster,
+                                alt : "Gotham Steel Sandwich Maker, Toaster and Electric Panini Grill with Ultra Nonstick Copper Surface - Makes 2 Sandwiches in Minutes with Virtually No Clean Up, with Easy Cut Edges and Indicator Lights",
+                                link : "/gp/product/B07MM2RVM5",
+                            }
+                        ]}
+                        date={"Sun, Feb 14"}
+                    />
+                    <Card_Three
                         title={"Shop top-rated fashion"}
                         img={desktop_small_big_card}
                         alt={"Gotham Steel Sandwich Maker, Toaster and Electric Panini Grill with Ultra Nonstick Copper Surface - Makes 2 Sandwiches in Minutes with Virtually No Clean Up, with Easy Cut Edges and Indicator Lights"}
                         link={"/history"}
                         linktxt={"Shop Now"}
                     />
-                    <section className="sub__layout__card__grid gift">
-                        <div className="sub__layout__inner">
-                            <h2 className="sub__layout__inner__header">
-                                <span className="sub__layout__inner__header__title">Looking for a gift?</span>
-                            </h2>
-                            <section className="sub__layout__inner__body">
-                                <p>Find personalized gift ideas for everyone on your list.</p>
-                                <p className="sub__layout__card__grid__footer">
-                                    <a href="/history">Explore Now</a>
-                                </p>
-                            </section>
-                        </div>
-                    </section>
+                    
+                    <Card_Four
+                        title={"Looking for a gift?"}
+                        text={"Find personalized gift ideas for everyone on your list."}
+                        catLink={"/gifts"}
+                    />
 
                     <hr className="card__break"/>
 
-                    <section className="sub__layout__card__grid grid__half">
-                        <div className="prime__video">
-                            <h2 className="prime__video__header">
-                                <span className="prime__video__title">Prime Video: Recommended for you</span>
-                                <span className="prime__video__maintitle">John Wick 3: Parabellum</span>
-                            </h2>
-                            <section className="prime__video__ui__card">
-                                <div className="prime__video__img"></div>
-                                <a href="/">
-                                    <div className="prime__video__imgLink">
-                                        <img className="prime__video__image" src="https://images-na.ssl-images-amazon.com/images/S/pv-target-images/d1df7bdf7c76eec04489aa7931026362fed5284766e091f85c8594851526cd3d._RI_V_TTW_QL40_AC_SL792_.jpg" alt="John Wick 3: Parabellum"/>
-                                    </div>
-                                    <div className="prime__gradient"></div>
-                                </a>
-                            </section>
-                            <div className="sub__layout__inner__timeline">
-                                <p className="sub__layout__card__grid__footer">
-                                    <a href="/history">Shop Now</a>
-                                </p>
-                            </div>
-                        </div>
-                    </section>
+                    <Card_Video
+                        title={"John Wick 3: Parabellum"}
+                        img={"https://images-na.ssl-images-amazon.com/images/S/pv-target-images/d1df7bdf7c76eec04489aa7931026362fed5284766e091f85c8594851526cd3d._RI_V_TTW_QL40_AC_SL792_.jpg"}
+                    />
 
-                    <Card_Two
+                    <Card_Three
                         title={"Shop our Pets store"}
                         img={desktop_deals_pets}
                         alt={"Shop our Pets store"}
@@ -128,7 +92,7 @@ const Home = (props) => {
                         linktxt={"See More"}
                     />
 
-                    <Card_Two
+                    <Card_Three
                         title={"Shop deals in Home & Kitchen"}
                         img={home_and_kitchen}
                         alt={"Shop deals in Home & Kitchen"}
