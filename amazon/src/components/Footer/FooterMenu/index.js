542ed00c541e6f9eaf605201886f4a9108be3f31
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2021-03-15 01:26:04 -0400

    Style upper footer

--- amazon/src/components/Footer/FooterMenu/index.js
+++ amazon/src/components/Footer/FooterMenu/index.js
@@ -2,29 +2,27 @@ import React from 'react'
 
 const FooterMenu = ({heading, menu}) => {
     return (
-        <section className="page__footer__menu">
-            <section className="page__footer__column">
-                <h4 className="page__footer__colHead">{heading}</h4>
-                <ul className="page__footer__menu">
-                    {menu &&
-                        menu.map((item, index) => (
-                            <li key={index}>
-                                <a
-                                    onClick={(e) => {
-                                        if (item.onClick) {
-                                            e.preventDefault();
-                                            item.onClick && item.onClick();
-                                        }
-                                    }}
-                                    href={`${item.href}`}
-                                >
-                                    {item.label}
-                                </a>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </section>
+        <section className="page__footer__column">
+            <h4 className="page__footer__colHead">{heading}</h4>
+            <ul className="page__footer__menu">
+                {menu &&
+                    menu.map((item, index) => (
+                        <li key={index} className="page__footer__list">
+                            <a
+                                onClick={(e) => {
+                                    if (item.onClick) {
+                                        e.preventDefault();
+                                        item.onClick && item.onClick();
+                                    }
+                                }}
+                                href={`${item.href}`}
+                            >
+                                {item.label}
+                            </a>
+                        </li>
+                    ))
+                }
+            </ul>
         </section>
     )
 }
