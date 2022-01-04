35e4eccf5cc750208282bd47bd89bdb73ca59365
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2022-01-04 12:50:53 -0500

    Create 'Amazon Web Services' page -- [Illustration]

--- amazon/src/pages/AmazonWebServices/index.js
+++ amazon/src/pages/AmazonWebServices/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { AiFillCaretDown, AiOutlineSearch } from 'react-icons/ai'
-
+import { AiFillCaretDown, AiOutlineSearch, AiOutlineDoubleRight } from 'react-icons/ai'
+import { Row, Col } from 'reactstrap'
 import Popover from '../../components/UI/Popover'
 
 import aws_poster_vid from '../../images/aws-poster-vid.png'
+import placeholder_agility from '../../images/placeholder-agility.png'
+import placeholder_flexibility from '../../images/placeholder-flexibility.png'
+import video_placeholder_cost_savings from '../../images/video-placeholder-cost-savings.png'
+import video_placeholder_global from '../../images/video-placeholder-global.png'
+import aws_types_of_cloud_computing_infrastructure from '../../images/aws-types-of-cloud-computing-infrastructure.png'
 
 import './index.css'
 
@@ -244,13 +249,217 @@ const AmazonWebServices = () => {
                         </section>
                     </section>
                 </section>
-                <section className="webServices__users">
+                <section className="webServices__pane">
                     <article className="webServices__container text-center">
-                        <h1 className="webServices__usersTitle">Who is using cloud computing?</h1>
-                        <p className="webServices__usersText">
-                            Cloud computing is the on-demand delivery of IT resources over the Internet with pay-as-you-go pricing. Instead of buying, owning, and maintaining physical data centers and servers, you can access technology services, such as computing power, storage, and databases, on an as-needed basis from a cloud provider like Amazon Web Services (AWS).
-                        </p>
+                        <section>
+                            <h1 className="webServices__paneTitle">Who is using cloud computing?</h1>
+                            <p className="webServices__usersText">
+                                Cloud computing is the on-demand delivery of IT resources over the Internet with pay-as-you-go pricing. Instead of buying, owning, and maintaining physical data centers and servers, you can access technology services, such as computing power, storage, and databases, on an as-needed basis from a cloud provider like Amazon Web Services (AWS).
+                            </p>
+                        </section>
                     </article>
+                </section>
+                <section className="webServices__pane">
+                    <article className="webServices__container text-center">
+                        <section className="webServices__benefits">
+                            <h1 className="webServices__paneTitle">Benefits of cloud computing</h1>
+                            <section className="webServices__bgPatternTop"></section>
+                            <section className="webServices__illustration d-flex justify-content-between">
+                                <section className="webServices__illustrator">
+                                    <figure>
+                                        <img src={placeholder_agility} alt="placeholder agility" />
+                                    </figure>
+                                </section>
+                                <section className="webServices__benefitTxt text-left">
+                                    <h2 className="websServices__benefitTitle">
+                                        Agility
+                                    </h2>
+                                    <section className="webServices__benefitPara">
+                                        <p>
+                                            The cloud gives you easy access to a broad range of technologies so that you can innovate faster and build nearly anything that you can imagine. You can quickly spin up resources as you need them–from infrastructure services, such as compute, storage, and databases, to Internet of Things, machine learning, data lakes and analytics, and much more.
+                                        </p>
+                                        <p>
+                                            You can deploy technology services in a matter of minutes, and get from idea to implementation several orders of magnitude faster than before. This gives you the freedom to experiment, test new ideas to differentiate customer experiences, and transform your business.<br />
+                                        </p>
+                                    </section>
+                                </section>
+                            </section>
+                            <section className="webServices__bgPattern"></section>
+                        </section>
+                    </article>
+                </section>
+                <section className="webServices__pane">
+                    <article className="webServices__container">
+                        <section className="webServices__benefits">
+                            <section className="webServices__illustration d-flex justify-content-between">
+                                <section className="webServices__benefitTxt text-left">
+                                    <h2 className="websServices__benefitTitle">
+                                        Elasticity
+                                    </h2>
+                                    <section className="webServices__benefitPara">
+                                        <p>
+                                            TWith cloud computing, you don’t have to over-provision resources up front to handle peak levels of business activity in the future. Instead, you provision the amount of resources that you actually need. You can scale these resources up or down to instantly grow and shrink capacity as your business needs change.
+                                        </p>
+                                    </section>
+                                </section>
+                                <section className="webServices__illustrator webServices__illustrator--secondary text-center">
+                                    <figure>
+                                        <img src={placeholder_flexibility} alt="placeholder flexibility" />
+                                    </figure>
+                                </section>
+                            </section>
+                        </section>
+                    </article>
+                </section>
+                <section className="webServices__pane">
+                    <article className="webServices__container text-center">
+                        <section className="webServices__benefits">
+                            <section className="webServices__bgPatternTop"></section>
+                            <section className="webServices__illustration d-flex justify-content-between align-items-center">
+                                <section className="webServices__illustrator">
+                                    <figure>
+                                        <img src={video_placeholder_cost_savings} alt="video placeholder cost savings" />
+                                    </figure>
+                                </section>
+                                <section className="webServices__benefitTxt text-left">
+                                    <h2 className="websServices__benefitTitle">
+                                        Cost savings
+                                    </h2>
+                                    <section className="webServices__benefitPara">
+                                        <p>
+                                            The cloud allows you to trade capital expenses (such as data centers and physical servers) for variable expenses, and only pay for IT as you consume it. Plus, the variable expenses are much lower than what you would pay to do it yourself because of the economies of scale. 
+                                        </p>
+                                    </section>
+                                </section>
+                            </section>
+                            <section className="webServices__bgPattern"></section>
+                        </section>
+                    </article>
+                </section>
+                <section className="webServices__pane">
+                    <article className="webServices__container">
+                        <section className="webServices__benefits">
+                            <section className="webServices__illustration d-flex justify-content-between">
+                                <section className="webServices__benefitTxt text-left">
+                                    <h2 className="websServices__benefitTitle">
+                                        Deploy globally in minutes
+                                    </h2>
+                                    <section className="webServices__benefitPara">
+                                        <p>
+                                            With the cloud, you can expand to new geographic regions and deploy globally in minutes. For example, AWS has infrastructure all over the world, so you can deploy your application in multiple physical locations with just a few clicks. Putting applications in closer proximity to end users reduces latency and improves their experience.
+                                        </p>
+                                    </section>
+                                </section>
+                                <section className="webServices__illustrator webServices__illustrator--secondary text-center">
+                                    <figure>
+                                        <img src={video_placeholder_global} alt="video placeholder global" />
+                                    </figure>
+                                </section>
+                            </section>
+                        </section>
+                    </article>
+                </section>
+                <section className="webServices__pane webServices__pane--list">
+                    <section className="webServices__container text-center">
+                        <section className="webServices__benefits">
+                            <section className="webServices__bgPatternTop"></section>
+                            <h1 className="webServices__paneTitle webServices__paneTitle--list">Types of cloud computing</h1>
+                            <article className="webServices__illustration">
+                                <p>
+                                    The three main types of cloud computing include Infrastructure as a Service, Platform as a Service, and Software as a Service. Each type of cloud computing provides different levels of control, flexibility, and management so that you can select the right set of services for your needs.
+                                </p>  
+                                <p>
+                                    <small>
+                                        <Link to="/">Learn more <AiOutlineDoubleRight/></Link>
+                                    </small>
+                                </p>
+                                {/* <section className="webServices__illustrator">
+                                    <figure>
+                                        <img src={video_placeholder_cost_savings} alt="video placeholder cost savings" />
+                                    </figure>
+                                </section>
+                                <section className="webServices__benefitTxt text-left">
+                                    <h2 className="websServices__benefitTitle">
+                                        Cost savings
+                                    </h2>
+                                    <section className="webServices__benefitPara">
+                                        <p>
+                                            The cloud allows you to trade capital expenses (such as data centers and physical servers) for variable expenses, and only pay for IT as you consume it. Plus, the variable expenses are much lower than what you would pay to do it yourself because of the economies of scale. 
+                                        </p>
+                                    </section> 
+                                </section>*/}
+                            </article>
+                            <article className="webServices__illustration d-flex">
+                                <Row>
+                                    <Col xs="3">
+                                        <figure>
+                                            <img src={aws_types_of_cloud_computing_infrastructure} alt="aws types of cloud computing infrastructure" />
+                                        </figure>
+                                    </Col>
+                                    <Col xs="9">
+                                        <h2 className="websServices__benefitTitle">Deploy globally in minutes</h2>
+                                        <section className="webServices__benefitPara">
+                                            <p>
+                                                With the cloud, you can expand to new geographic regions and deploy globally in minutes. For example, AWS has infrastructure all over the world, so you can deploy your application in multiple physical locations with just a few clicks. Putting applications in closer proximity to end users reduces latency and improves their experience.
+                                            </p>
+                                        </section>
+                                    </Col>
+                                </Row>
+                            </article>
+                            <article className="webServices__illustration d-flex">
+                                <Row>
+                                    <Col xs="3">
+                                        <figure>
+                                            <img src={aws_types_of_cloud_computing_infrastructure} alt="aws types of cloud computing infrastructure" />
+                                        </figure>
+                                    </Col>
+                                    <Col xs="9">
+                                        <h2 className="websServices__benefitTitle">Deploy globally in minutes</h2>
+                                        <section className="webServices__benefitPara">
+                                            <p>
+                                                With the cloud, you can expand to new geographic regions and deploy globally in minutes. For example, AWS has infrastructure all over the world, so you can deploy your application in multiple physical locations with just a few clicks. Putting applications in closer proximity to end users reduces latency and improves their experience.
+                                            </p>
+                                        </section>
+                                    </Col>
+                                </Row>
+                            </article>
+                            <article className="webServices__illustration d-flex">
+                                <Row>
+                                    <Col xs="3">
+                                        <figure>
+                                            <img src={aws_types_of_cloud_computing_infrastructure} alt="aws types of cloud computing infrastructure" />
+                                        </figure>
+                                    </Col>
+                                    <Col xs="9">
+                                        <h2 className="websServices__benefitTitle">Deploy globally in minutes</h2>
+                                        <section className="webServices__benefitPara">
+                                            <p>
+                                                With the cloud, you can expand to new geographic regions and deploy globally in minutes. For example, AWS has infrastructure all over the world, so you can deploy your application in multiple physical locations with just a few clicks. Putting applications in closer proximity to end users reduces latency and improves their experience.
+                                            </p>
+                                        </section>
+                                    </Col>
+                                </Row>
+                            </article>
+                            <article className="webServices__illustration d-flex">
+                                <Row>
+                                    <Col xs="3">
+                                        <figure>
+                                            <img src={aws_types_of_cloud_computing_infrastructure} alt="aws types of cloud computing infrastructure" />
+                                        </figure>
+                                    </Col>
+                                    <Col xs="9">
+                                        <h2 className="websServices__benefitTitle">Deploy globally in minutes</h2>
+                                        <section className="webServices__benefitPara">
+                                            <p>
+                                                With the cloud, you can expand to new geographic regions and deploy globally in minutes. For example, AWS has infrastructure all over the world, so you can deploy your application in multiple physical locations with just a few clicks. Putting applications in closer proximity to end users reduces latency and improves their experience.
+                                            </p>
+                                        </section>
+                                    </Col>
+                                </Row>
+                            </article>
+                            <section className="webServices__bgPattern"></section>
+                        </section>
+                    </section>
                 </section>
             </main>
         </section>
