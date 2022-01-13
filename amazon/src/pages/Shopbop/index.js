1f12cdc3626a27a4baa48b8908894613b633f4e3
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2022-01-13 16:05:58 -0500

    Create 'Amazon Web Services' page [Nav]

--- amazon/src/pages/Shopbop/index.js
+++ amazon/src/pages/Shopbop/index.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom'
 
 import { FaMapMarkerAlt } from 'react-icons/fa'
 import { MdKeyboardArrowDown } from 'react-icons/md'
+import { AiOutlineSearch } from 'react-icons/ai'
 
 import rebrand_hearts from '../../images/rebrand-hearts.png'
 import shopbop_bag from '../../images/shopbop-bag.png'
+import rebrand_shopbop_logo from '../../images/rebrand-shopbop-logo.png'
 
 import './index.css'
 
@@ -48,6 +50,72 @@ const Shopbop = () => {
                             <div className="shopbop__cartCount">0</div>
                         </section>
                     </section>
+                </section>
+                <section className="shopbop__nav">
+                    <section className="shopbop__logo">
+                        <Link to="/">
+                            <img src={rebrand_shopbop_logo} alt="rebrand shopbop logo" />
+                        </Link>
+                    </section>
+                    <nav className="shopbop__navList">
+                        <ul>
+                            <li>
+                                <Link to="/">
+                                    What's New
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    Our Favorites
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    Designers
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    Clothing
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    Active
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    Shoes
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    Bags
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    Accessories
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    Men
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to="/">
+                                    Sale
+                                </Link>
+                            </li>
+                            <li>
+                                <button className="shopbop__search">
+                                    <AiOutlineSearch />
+                                </button>
+                            </li>
+                        </ul>
+                    </nav>
                 </section>
             </header>
         </div>
